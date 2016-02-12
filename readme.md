0537bcc644fa225c8ff5591c2011a7b717294efa
Mamat Rahmat <coderbodoh@gmail.com>
2016-02-13 03:19:59 +0700

    first commit

---
+++ readme.md
@@ -0,0 +1,38 @@
+# TLC Backup
+Script [Python](https://www.python.org) untuk melakukan backup submission yang Accepted dari [Tokilearning](http://tokilearning.org/).
+
+Dikarenakan tokilearning alias TLC akan dinonaktifkan, penting bagi pengguna untuk melakukan backup submission.
+
+## Penggunaan
+
+1. Jika anda belum memiliki python, [Download Python](https://www.python.org/downloads/) versi 3.x.y
+2. Pastikan python dan pip dapat diakses cari command line (cmd/terminal)
+
+    ```
+    python -V
+    ```
+
+    ```
+    pip -V
+    ```
+
+3. Install dependensi script
+    ```
+    pip install requests, beautifulsoup4
+    ```
+4. Login ke tokilearning. Simpan PHPSESSID dengan klik kanan -> View Page Info -> Security -> View Cookies -> Ambil bagian Content untuk Name : PHPSESSID. Nilai berupa sebuah string acak.
+
+5. Download script
+
+    a. Jika anda pengguna git. Clone repo ini
+
+    b. Jika anda bukan pengguna git. Script dapat di download dengan mengeklik tombol Download ZIP di kanan atas, lalu extract ke suatu direktori
+
+6. Masuk ke direktori penyimpanan script dengan command line (cmd/terminal). Jalankan script dengan memasukkan PHPSESSID yang diperoleh
+
+    ```
+    python tlc_backup.py your_PHPSESSID
+    ```
+
+## Lain-lain
+Terinspirasi dari [GTL](https://github.com/matematikaadit/gtl)
---
+++ tlc_backup.py
@@ -0,0 +1,49 @@
+import requests
+import platform
+import sys
+from bs4 import BeautifulSoup
+
+if(len(sys.argv) != 2):
+    print('Wrong arguments. Usage : python tlc_backup.py your_PHPSESSID')
+else:
+    phpsessid = sys.argv[1]
+    cookies = {'PHPSESSID' : phpsessid}
+    submission_url = 'http://tokilearning.org/submission/'
+    home_response = requests.get(submission_url, cookies=cookies)
+    home_html = home_response.text
+    soup = BeautifulSoup(home_html, 'html.parser')
+    last = soup.find('li', class_='last').a['href'][-2:]
+
+    for i in range(1, int(last)+1):
+        print('parsing page ', i);
+        response = requests.get(submission_url + 'index?ajax=submissiongridview&Submission_page=' + str(i), cookies=cookies)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        submission_rs = soup.find_all('tr', class_=['odd', 'even'])
+
+        for element in submission_rs:
+            submission = element.contents
+            verdict = submission[3].contents[0]
+
+            if(verdict == 'Accepted') :
+                problem = submission[0].a
+                problem_id = problem['href'][9:]
+                problem_name = problem.contents[0].replace('?', '')
+                answer = submission[4].a
+                answer_id = answer['href'][12:]
+                print('> found solution to problem', problem_id, problem_name, 'with ID', answer_id)
+                
+                response = requests.get(submission_url + answer_id + '?action=download', cookies=cookies)
+                content_disposition = response.headers['content-disposition']
+                if(content_disposition[-2:-1] == 'c'):
+                    lang = 'c'
+                else:
+                    lang = response.headers['content-disposition'][-4:-1]
+                content = response.text
+
+                if platform.platform().startswith('Windows'):
+                    content = content.replace('\r\n', '\n')
+                
+                with open('{0}_{1}_{2}.{3}'.format(problem_id, problem_name, answer_id, lang), 'w') as text_file:
+                    text_file.write(content)
